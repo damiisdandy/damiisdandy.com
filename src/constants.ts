2f16b69e2c9c09cd2fa2ea956748d1ec5bcb4b7a
Damilola Jerugba <hello@damiisdandy.com>
2024-06-12 11:51:58 +0100

    updated careers page

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const TWITTER = "https://twitter.com/realdamiisdandy";
 export const CALENDLY = "https://calendly.com/damiisdandy/get-to-know-me-1";
 export const SPOTIFY_PLAYLIST_ID = "2qFyGGYI4BTTB6x4ogvUep";
 export const RESUME =
-  "https://docs.google.com/document/d/18dWuvsgkwuxS_7dwBgd4mySDffHpp-f-jmUFZFm-KDk/edit?usp=sharing";
+  "https://docs.google.com/document/d/11eHPR5xHXgEcaBZglNqNx1ahNCkddvpnh2-EhoDJrLI/edit?usp=sharing";
