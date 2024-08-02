bcf56b47798262b0721a7fe8533c26cc17dd6d34
Damilola Jerugba <hello@damiisdandy.com>
2024-08-02 17:18:57 +0100

    updated resume link

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,6 @@ export const LINKEDIN = "https://www.linkedin.com/in/damiisdandy";
 export const CALENDLY = "https://calendly.com/damiisdandy/get-to-know-me-1";
 export const SPOTIFY_PLAYLIST_ID = "2qFyGGYI4BTTB6x4ogvUep";
 export const RESUME =
-  "https://docs.google.com/document/d/1mX0sknbillOSJUGlTl8jyuZimIxfNww87MuCKjnQMHw";
+  "https://docs.google.com/document/d/1aqU9vcMwvttY29iG6DqvU9wppjVVxytcyqUvZUmrBV4";
 export const PLACE_HOLDER_IMAGE =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAScAAACUAQMAAADrvX1tAAAAA1BMVEUKCgqw17mYAAAAHElEQVRYw+3BAQ0AAADCIPuntscHDAAAAACAlAMV+AABq9uF5gAAAABJRU5ErkJggg==";
